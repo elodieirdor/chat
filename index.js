581027a809c704ca81a036de48ab641baa8f300f
elodie.irdor <elodie.irdor@voyager.nz>
2021-04-03 04:30:37 +1300

    Render link / image

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
-const http = require('http').createServer(app);
+const https = require('https').createServer({
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.cert')
+  }, app);
 // create a new instance of socket.io
-const io = require('socket.io')(http);
+const io = require('socket.io')(https);
+const axios = require('axios');
+const HTMLParser = require('node-html-parser');
 
 const getRandomInt = (max = 99) => {
     return Math.floor(Math.random() * max);
@@ -12,6 +18,43 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/info-url', async (req, res) => {
+    const content = {type: 'html', title: null, image: null};
+    
+    // TODO clean this
+    console.log(req.query.url);
+    try {
+        const response = await axios.get(req.query.url);
+        const isImage = response.headers['content-type'].indexOf('image') != -1;
+        if (isImage) {
+            content.type = 'img';
+        } else {
+            var root = HTMLParser.parse(response.data);
+            
+            // title
+            var metaTitle = root.querySelector('meta[property="og:title"]');
+            if (metaTitle) {
+                content.title = metaTitle.getAttribute('content');
+            } else {
+                metaTitle = root.querySelector('title');
+                if (metaTitle) {
+                    content.title = metaTitle.text;
+                }
+            }
+
+            // image 
+            var metaImage = root.querySelector('meta[property="og:image"]');
+            if (metaImage) {
+                content.image = metaImage.getAttribute('content');
+            }
+        }
+    } catch (error) {
+        console.log(error);
+    }
+
+    res.send(content);
 });
 
 // io.emit to send a message to all connected clients
@@ -37,6 +80,6 @@ io.on('connection', (socket) => {
     });
 });
 
-http.listen(3000, () => {
+https.listen(3000, () => {
     console.log('listening on *:3000');
 });
--- public/js/app.js
+++ public/js/app.js
@@ -9,6 +9,85 @@ usernameInput.focus();
 
 var messageInput = document.getElementById('input');
 
+/*!
+ * Sanitize and encode all HTML in a user-submitted string
+ * (c) 2018 Chris Ferdinandi, MIT License, https://gomakethings.com
+ * @param  {String} str  The user-submitted string
+ * @return {String} str  The sanitized string
+ */
+const sanitizeHTML = (str) => {
+    var temp = document.createElement('div');
+    temp.textContent = str;
+    return temp.innerHTML;
+};
+
+const renderHtmlForLink = async (match, _message) => {
+    var url = match;
+
+
+    if (match.indexOf('http', 0) === -1) {
+        url = `https://${url}`;
+    }
+
+    try {
+
+    // get info from url
+    var response = await fetch(`/info-url?url=${url}`);
+        var json = await response.json();
+
+        // TODO use templates
+
+        if (json.type === 'img') {
+            return _message.replace(match,
+                `<a target="_blank" class="underline" href="${url}">
+                    <img src="${url}" width="200" height="200"/>
+                </a>`
+            );
+        } else {
+            if (json.type === 'html') {
+
+                if (json.image) {
+                    return _message.replace(match, 
+                        `<div class="my-5">
+                            <a target="_blank" class="underline" href="${url}">
+                                <div>${json.title ?? match}</div>
+                                <img src="${json.image}" width="200" height="200"/>
+                            </a>
+                        </div>`
+                    );
+                }
+
+                return _message.replace(match, `<a target="_blank" class="underline" href="${url}">${json.title ?? match}</a>`);
+            } 
+        }
+    } catch (error) {
+        console.log(`Request failed ${error}`);
+    }
+
+    return _message.replace(match, `<a target="_blank" class="underline" href="${url}">${match}</a>`);
+};
+
+const transformLink = async (message) => {
+    var _message = message;
+    var regexUrl = /(http:\/\/|https:\/\/)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gi;
+    var matches = _message.match(regexUrl);
+    if (matches !== null) {
+        for (var match of matches) {
+            const result = await renderHtmlForLink(match, _message);
+            _message = result;
+        }
+    }
+
+    return _message;
+};
+
+const transformMessage = (message) => {
+    var sanitizedMsg = sanitizeHTML(message);
+    var updatedMessage = transformLink(sanitizedMsg);
+
+    return updatedMessage;
+};
+
 const addMessageToList = (messageEl) => {
     messages.appendChild(messageEl);
     window.scrollTo(0, document.body.scrollHeight);
@@ -16,7 +95,7 @@ const addMessageToList = (messageEl) => {
 
 const addMyMessage = (message) => {
     var clone = document.importNode(templateMyMessage.content, true);
-    clone.querySelector('[data-message]').textContent = message;
+    clone.querySelector('[data-message]').innerHTML = message;
     addMessageToList(clone);
 };
 
@@ -24,7 +103,7 @@ const addOtherUserMessage = (user, message) => {
     var clone = document.importNode(templateMessage.content, true);
     clone.querySelector('img').setAttribute('src', user.image);
     clone.querySelector('[data-username]').textContent = user.username;
-    clone.querySelector('[data-message]').textContent = message;
+    clone.querySelector('[data-message]').innerHTML = message;
     addMessageToList(clone);
 }
 
@@ -58,11 +137,12 @@ usernameForm.addEventListener('submit', function (e) {
 });
 
 var form = document.getElementById('form');
-form.addEventListener('submit', function (e) {
+form.addEventListener('submit', async function (e) {
     e.preventDefault();
     if (messageInput.value) {
-        socket.emit('chat message', messageInput.value);
-        addMyMessage(messageInput.value);
+        var _message = await transformMessage(messageInput.value);
+        socket.emit('chat message', _message);
+        addMyMessage(_message);
         messageInput.value = '';
     }
 });
